ab246eef71843a4de6e4c6dbc9ace2701039e1aa
Devan Botheras <1016781@student.sae.edu.au>
2022-02-16 16:47:36 +1100

    Started mouse interactions

--- Assets/Scripts/MouseInteraction.cs
+++ Assets/Scripts/MouseInteraction.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MouseInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameManager gameManager;
+
+    public GameObject winCanvas;
+
+    private bool isClicked;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject() && isClicked == false)
+        {
+            isClicked = true;
+        }
     }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public float time;
     public bool timerEnd;
 
+    private Slider timerSlider;
+
     // Start is called before the first frame update
     void Start()
     {
         time = 10f;
+        timerSlider = GetComponent<Slider>();
 
         StartCoroutine(TimerCountdown());
     }
 
     IEnumerator TimerCountdown()
     {
-        return new WaitForSecondsRealtime(1);
-        time -= 1;
+        yield return new WaitForSecondsRealtime(1);
 
+        time -= 1;
+        timerSlider.value = time;
         TimeCheck();
     }
 
